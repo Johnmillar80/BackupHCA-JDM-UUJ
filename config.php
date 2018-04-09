208041d99af634957593bb0262f775217ee8d14c
Johnmillar80 <37422395+Johnmillar80@users.noreply.github.com>
2018-04-09 16:58:11 +0100

    26.03.18

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "health_chat_anonymous";
+	$tablename = "calendarsystem";
+
+	// translate these
+	$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+	$headings = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+?>
